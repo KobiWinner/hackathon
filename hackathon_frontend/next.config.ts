561fbb0b4400d435ea64b9a72bc423faa12da02e
Hüseyin Genç <huseyingenc.eem@gmail.com>
2025-12-16 14:59:55 +0300

    feat: add initial Docker setup, CI/CD pipeline, and environment configuration

--- hackathon_frontend/next.config.ts
+++ hackathon_frontend/next.config.ts
@@ -1,7 +1,22 @@
+// import bundleAnalyzer from "@next/bundle-analyzer";
+
 import type { NextConfig } from "next";
 
+// const withBundleAnalyzer = bundleAnalyzer({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
+// export default withBundleAnalyzer(nextConfig);
